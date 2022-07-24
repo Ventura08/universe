309b5553e31f072f45f73003c7ca3b3b0fd826df
Ventura08 <estevaotevu08@gmail.com>
2022-07-24 14:33:17 -0300

    ante do babao

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export async function getPlanets() {
     const res = await fetch("https://api.le-systeme-solaire.net/rest/bodies");
     const json = await res.json();
-    console.log(json);
+    // console.log(json);
     return json;
 }
 
