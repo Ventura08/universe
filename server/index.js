df89772887d6bf63e011b406316c333d593ab593
barbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-08-07 13:18:50 -0300

    ajustes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 import express from "express";
-import { getPlanets } from "../api.js";
 import Moons from "./models/database/moons.json" assert {type: "json"};
 import { deleteMoon } from "./controllers/moon.js";
-
-const planets = await getPlanets();
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +8,6 @@ const app = express();
   app.use(express.json());
 
   app.get("/moons", (req, res) => {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     res.json({Moons});
   });
 
